553647b26f8f4ed2852f99a15b21233e2e33a5b8
Sathabhron <thahi5_vi@hotmail.com>
2021-08-16 01:42:54 +0700

    Update binary_search.cpp
    Add counter glow up rate of algorithm.

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,19 +1,22 @@
-
 #include <iostream>
 
 using namespace std;
 
-int binarySearch(int arr[], int left, int right, int find)
+int binarySearch(int arr[], int left, int right, int find, int counter)
 {
     if (right > 1) {
         int mid;
+        
+        counter++; // ไม่จำเป็นเเค่อยากนับว่าทำไปกี่ครั้งเเล้ว
+        cout << "O(n) = " << counter << "\n";
+        
         mid = left + (right - left) / 2;
         if (arr[mid] == find) {
             return mid;
         }else if (arr[mid] > find) {
-            return binarySearch(arr, left, mid-1, find);
+            return binarySearch(arr, left, mid-1, find, counter);
         }else{
-            return binarySearch(arr, mid+1, right, find);
+            return binarySearch(arr, mid+1, right, find, counter);
         }
     }else{
         return -1;
@@ -26,7 +29,7 @@ int main()
     int n = sizeof(arr)/sizeof(arr[0]);
     int find = 15;
     int i;
-    int result = binarySearch(arr, 0, n - 1, find);
+    int result = binarySearch(arr, 0, n - 1, find, 0);
     cout << "Found in position : " << result << " ,Result is : " << arr[result];
 
     return 0;
